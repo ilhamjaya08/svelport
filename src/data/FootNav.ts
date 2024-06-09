114c5da5f5246c4467c202654f90270580bd2734
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-06-10 07:44:11 +0800

    refactor: change footer navigation title to arrayed Json

---
+++ src/data/FootNav.ts
@@ -0,0 +1,69 @@
+const Contact = [
+    {
+        title: "Instagram",
+        href: "https://instagram.com/ilham.j_08"
+    },
+    {
+        title: "Telegram",
+        href: "https://t.me/@ilhamjaya08"
+    },
+    {
+        title: "Discord",
+        href: "https://discord.gg/AdgwXdmBqC"
+    },
+    {
+        title: "Others",
+        href: "https://linktr.ee/ilhamjaya08"
+    }
+]
+
+const Page = [
+    {
+        title: "About",
+        href: "/about"
+    },
+    {
+        title: "Contact",
+        href: "/contact"
+    },
+    {
+        title: "Resume/CV",
+        href: "/resume,"
+    },
+    {
+        title: "Works",
+        href: "/works"
+    }
+]
+
+const Others = [
+    {
+        title: "Order an app",
+        href: "https://t.me/@ilhamjaya08"
+    },
+    {
+        title: "Hire me",
+        href: "https://t.me/@ilhamjaya08"
+    }
+]
+
+const Icon = [
+    {
+        alt: "GitHub",
+        icon: "flowbite:github-solid",
+        href: "https://github.com/ilhamjaya08",
+    },
+    {
+        alt: "Discord",
+        icon: "flowbite:discord-solid",
+        href: "https://discord.gg/AdgwXdmBqC",
+    },
+    {
+        alt: "Instagram",
+        icon: "flowbite:instagram-solid",
+        href: "https://instafram.com/ilham.j_08",
+    }
+]
+
+
+export { Contact, Page, Others, Icon }
--- src/lib/components/Foot.svelte
+++ src/lib/components/Foot.svelte
@@ -1,6 +1,7 @@
 <script>
     import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, FooterBrand, FooterIcon } from 'flowbite-svelte';
     import Icon from '@iconify/svelte';
+    import { Contact, Page, Others, Icon as icons } from '../../data/FootNav';
   </script>
   
   <Footer footerType="socialmedia" class="border-t-2 border-gray-200 mt-4">
@@ -12,26 +13,25 @@
         <div>
           <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact</h2>
           <FooterLinkGroup>
-            <FooterLink liClass="mb-4" href="https://instagram.com/ilham.j_08">Instagram</FooterLink>
-            <FooterLink liClass="mb-4" href="t.me/@ilhamjaya08">Telegram</FooterLink>
-            <FooterLink liClass="mb-4" href="https://discord.gg/AdgwXdmBqC">Discord</FooterLink>
-            <FooterLink liClass="mb-4" href="https://linktr.ee/ilhamjaya08">Others</FooterLink>
+            {#each Contact as contact}
+            <FooterLink liClass="mb-4" href={contact.href}>{contact.title}</FooterLink>
+            {/each}
           </FooterLinkGroup>
         </div>
         <div>
           <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Page</h2>
           <FooterLinkGroup>
-            <FooterLink liClass="mb-4" href="/about">About</FooterLink>
-            <FooterLink liClass="mb-4" href="/works">Works</FooterLink>
-            <FooterLink liClass="mb-4" href="/contact">Contact</FooterLink>
-            <FooterLink liClass="mb-4" href="/resume">Resume/CV</FooterLink>
+            {#each Page as page}
+            <FooterLink liClass="mb-4" href={page.href}>{page.title}</FooterLink>
+            {/each}
           </FooterLinkGroup>
         </div>
         <div>
           <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Others</h2>
           <FooterLinkGroup>
-            <FooterLink liClass="mb-4" href="https://t.me/@ilhamjaya08">Order an app</FooterLink>
-            <FooterLink liClass="mb-4" href="https://t.me/@ilhamjaya08">Hire me</FooterLink>
+            {#each Others as others}
+            <FooterLink liClass="mb-4" href={others.href}>{others.title}</FooterLink>
+            {/each}
           </FooterLinkGroup>
         </div>
       </div>
@@ -40,18 +40,11 @@
     <div class="sm:flex sm:items-center sm:justify-between">
       <FooterCopyright href="/" by="Muhammad Ilham Jaya " />
       <div class="flex mt-4 space-x-6 rtl:space-x-reverse sm:justify-center sm:mt-0">
-        <FooterIcon href="/">
-          <Icon icon="flowbite:github-solid" class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+        {#each icons as icon}
+        <FooterIcon href={icon.href}>
+          <Icon icon={icon.icon} class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
         </FooterIcon>
-        <FooterIcon href="/">
-          <Icon icon="flowbite:discord-solid" class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-        </FooterIcon>
-        <FooterIcon href="/">
-          <Icon icon="flowbite:instagram-solid" class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-        </FooterIcon>
-        <FooterIcon href="/">
-          <Icon icon="flowbite:facebook-solid" class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-        </FooterIcon>
+        {/each}
       </div>
     </div>
   </Footer>
